15a73724c2d43a950b78ae69e14d91b796954632
rahuljauhari2 <rahuljauhari02@gmail.com>
2024-12-05 21:07:09 +0530

    Question updated

--- JavaDsa/Dll.java
+++ JavaDsa/Dll.java
@@ -1,3 +1,26 @@
+/*
+ * Assignment-4: Doubly Linked List
+
+1. Write a Java class with the name DLL, with the instance variables start to hold reference of type node. Define Node as inner class with instance member variables item, prev and next reference variables of type Node. The item variable is to hold data and prev & next are reference variables to refer the previous and next node. Also define setters and getters in Node class.
+
+2. Define a method to check if the doubly linked list is empty.
+
+3. Define a method to insert a new node at the beginning of the list.
+
+4. Define a method to insert a new node at the end of the list.
+
+5. Define a method to search a node of given item value
+
+6. Define a method to insert a new node after a given node.
+
+7. Define a method to delete first node of the list.
+
+8. Define a method to delete last node of the list.
+
+9. Define a method to delete a node with given item value. If, multiple nodes of same item value found then delete the first occurrence node
+ */
+
+
 public class Dll {
     public static void main(String[]args){
         List obj = new List();
